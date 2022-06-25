707def2e0bbd5891eed9402896c9c3b9e737fba4
商 黄 <3183964336@qq.com>
2022-06-25 11:04:57 +0800

    添加项目文件。

---
+++ list容器基本概念和构造函数.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <string.h>
+#include <list>
+using namespace std;
+/*
+STL中实现的是一个双向循环链表，将数据进行链式存储
+链表是一种物理存储单元上非连续的存储结构，数据元素的逻辑顺序通过链表中的指针链接实现
+
+*/
+void test01()
+{
+
+}
+int main()
+{
+
+	system("pause");
+	return 0;
+}
+
